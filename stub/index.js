afcb35f3c5a81d33a1cf7954d961dbde5184cd7f
nuralimov <n.alimov@indriver.com>
2020-01-18 12:27:47 +0300

    init commit

---
+++ stub/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const {bemhtml} = require('bem-xjst');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+const templates = bemhtml.compile();
+
+app.use('/static', express.static(`${__dirname}/build`));
+
+app.get('/index|product', (req, res) => {
+    const page = req.path.replace('/', '');
+
+    const jsonPath = path.resolve(__dirname, 'pages', `${page}.json`);
+    const bemjson = JSON.parse(fs.readFileSync(jsonPath, {encoding: 'utf-8'}));
+
+    const contentHTML = templates.apply(bemjson);
+
+    const pageHTML = `
+    <!doctype html>
+    <html lang="ru">
+        <head>
+            <link rel="stylesheet" href="/static/style.css">
+            <title>Any title</title>
+        </head>
+        <body>
+            ${contentHTML}
+            <script type="text/javascript" src="/static/script.js"></script>
+        </body>
+    </html>`;
+
+    res.send(pageHTML);
+});
+
+app.listen(3000, () => {
+    console.log('App is listening on port 3000');
+});
